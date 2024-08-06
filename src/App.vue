90667a5dda3999fb039d9d5a86e6d83fd3d9d51b
Javier Grau <javiergrau.freelance@gmail.com>
2024-08-06 12:28:41 -0400

    Añadir logo de portal asesores

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app" class="flex flex-col items-center gap-11 bg-partner-color px-5 py-11">
+    <div>
+      <img class="max-w-52" src="./public/logo-portal-asesores.png" />
+    </div>
     <MyForm />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import MyForm from './components/MyForm.vue';
+import { defineComponent } from 'vue'
+import MyForm from './components/MyForm.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     MyForm
   }
-});
+})
 </script>
