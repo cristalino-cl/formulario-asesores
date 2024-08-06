f7fff25359e04705729073584aecc22b35d95094
Javier Grau <javiergrau.freelance@gmail.com>
2024-08-06 12:29:06 -0400

    Añadir styling con tailwind al formulario

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="form-container">
-    <h2 class="form-title">Reporte de Problemas</h2>
-    <form @submit.prevent="handleSubmit">
-      <div class="form-group">
-        <label for="deviceType">Tipo de dispositivo:</label>
-        <select id="deviceType" v-model="form.deviceType" required>
+  <div
+    class="flex w-full flex-col items-center gap-5 rounded-[3.5px] bg-black bg-opacity-25 p-5 text-sm text-white md:w-10/12 lg:w-8/12"
+  >
+    <h2 class="text-2xl">Reporte de Problemas</h2>
+    <form class="w-full space-y-8" @submit.prevent="handleSubmit">
+      <div class="flex flex-col">
+        <label class="mb-2" for="deviceType">Tipo de dispositivo</label>
+        <select
+          id="deviceType"
+          class="w-full rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          v-model="form.deviceType"
+          required
+        >
           <option value="">Seleccione una opción</option>
           <option value="PC">PC</option>
           <option value="Mac">Mac</option>
@@ -12,13 +19,25 @@
           <option value="Android">Android</option>
         </select>
       </div>
-      <div class="form-group">
-        <label for="deviceModel">Modelo del dispositivo:</label>
-        <input type="text" id="deviceModel" v-model="form.deviceModel" required />
+      <div class="flex flex-col">
+        <label class="mb-2" for="deviceModel">Modelo del dispositivo</label>
+        <input
+          class="rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          type="text"
+          id="deviceModel"
+          v-model="form.deviceModel"
+          required
+        />
       </div>
-      <div class="form-group">
-        <label for="operatingSystem">Sistema operativo:</label>
-        <select id="operatingSystem" v-model="form.operatingSystem" @change="checkOtherOS" required>
+      <div class="flex flex-col">
+        <label class="mb-2" for="operatingSystem">Sistema operativo</label>
+        <select
+          id="operatingSystem"
+          class="w-full rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          v-model="form.operatingSystem"
+          @change="checkOtherOS"
+          required
+        >
           <option value="">Seleccione una opción</option>
           <option value="Windows 11">Windows 11</option>
           <option value="macOS Ventura">macOS Ventura</option>
@@ -26,35 +45,79 @@
           <option value="Android 13">Android 13</option>
           <option value="Other">Otro</option>
         </select>
-        <input v-if="isOtherOS" type="text" v-model="form.otherOperatingSystem" placeholder="Ingrese el sistema operativo" required />
+        <input
+          class="mt-2 rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          v-if="isOtherOS"
+          type="text"
+          v-model="form.otherOperatingSystem"
+          placeholder="Ingrese el sistema operativo"
+          required
+        />
       </div>
-      <div class="form-group">
-        <label for="appVersion">Versión de la aplicación:</label>
-        <input type="text" id="appVersion" v-model="form.appVersion" required />
+      <div class="flex flex-col">
+        <label class="mb-2" for="appVersion">Versión de la aplicación</label>
+        <input
+          class="rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          type="text"
+          id="appVersion"
+          v-model="form.appVersion"
+          required
+        />
       </div>
-      <div class="form-group">
-        <label for="incidentDate">Fecha y hora del incidente:</label>
-        <input type="datetime-local" id="incidentDate" v-model="form.incidentDate" required />
+      <div class="flex flex-col">
+        <label class="mb-2" for="incidentDate">Fecha y hora del incidente</label>
+        <input
+          class="rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          type="datetime-local"
+          id="incidentDate"
+          v-model="form.incidentDate"
+          required
+        />
       </div>
-      <div class="form-group">
-        <label for="problemDescription">Descripción detallada del problema:</label>
-        <textarea id="problemDescription" v-model="form.problemDescription" required></textarea>
+      <div class="flex flex-col">
+        <label class="mb-2" for="problemDescription">Descripción detallada del problema</label>
+        <textarea
+          class="min-h-20 rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          id="problemDescription"
+          v-model="form.problemDescription"
+          required
+        ></textarea>
       </div>
-      <div class="form-group">
-        <label for="reproduccionSteps">Pasos para reproducir el problema:</label>
-        <textarea id="reproduccionSteps" v-model="form.reproduccionSteps" required></textarea>
+      <div class="flex flex-col">
+        <label class="mb-2" for="reproduccionSteps">Pasos para reproducir el problema</label>
+        <textarea
+          class="min-h-20 rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          id="reproduccionSteps"
+          v-model="form.reproduccionSteps"
+          required
+        ></textarea>
       </div>
-      <div class="form-group">
-        <label for="expectedBehavior">Comportamiento esperado:</label>
-        <textarea id="expectedBehavior" v-model="form.expectedBehavior" required></textarea>
+      <div class="flex flex-col">
+        <label class="mb-2" for="expectedBehavior">Comportamiento esperado</label>
+        <textarea
+          class="min-h-20 rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          id="expectedBehavior"
+          v-model="form.expectedBehavior"
+          required
+        ></textarea>
       </div>
-      <div class="form-group">
-        <label for="actualBehavior">Comportamiento actual:</label>
-        <textarea id="actualBehavior" v-model="form.actualBehavior" required></textarea>
+      <div class="flex flex-col">
+        <label class="mb-2" for="actualBehavior">Comportamiento actual</label>
+        <textarea
+          class="min-h-20 rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          id="actualBehavior"
+          v-model="form.actualBehavior"
+          required
+        ></textarea>
       </div>
-      <div class="form-group">
-        <label for="severityLevel">Nivel de severidad:</label>
-        <select id="severityLevel" v-model="form.severityLevel" required>
+      <div class="flex flex-col">
+        <label class="mb-2" for="severityLevel">Nivel de severidad</label>
+        <select
+          id="severityLevel"
+          class="w-full rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          v-model="form.severityLevel"
+          required
+        >
           <option value="">Seleccione una opción</option>
           <option value="low">Bajo</option>
           <option value="medium">Medio</option>
@@ -62,45 +125,69 @@
           <option value="critical">Crítico</option>
         </select>
       </div>
-      <div class="form-group">
-        <label for="hasPreviouslyOccurred">¿Ha ocurrido este problema antes?</label>
-        <select id="hasPreviouslyOccurred" v-model="form.hasPreviouslyOccurred" required>
+      <div class="flex flex-col">
+        <label class="mb-2" for="hasPreviouslyOccurred">¿Ha ocurrido este problema antes?</label>
+        <select
+          id="hasPreviouslyOccurred"
+          class="w-full rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          v-model="form.hasPreviouslyOccurred"
+          required
+        >
           <option value="">Seleccione una opción</option>
           <option value="yes">Sí</option>
           <option value="no">No</option>
         </select>
       </div>
-      <div class="form-group">
-        <label for="additionalComments">¿Algún otro comentario o información adicional?</label>
-        <textarea id="additionalComments" v-model="form.additionalComments"></textarea>
+      <div class="flex flex-col">
+        <label class="mb-2" for="additionalComments"
+          >¿Algún otro comentario o información adicional?</label
+        >
+        <textarea
+          class="min-h-20 rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          id="additionalComments"
+          v-model="form.additionalComments"
+        ></textarea>
       </div>
-      <div class="form-group">
-        <label for="screenshots">Capturas de pantalla o videos:</label>
-        <input type="file" id="screenshots" multiple @change="handleFileUpload" />
+      <div class="flex flex-col">
+        <label class="mb-2" for="screenshots">Capturas de pantalla o videos</label>
+        <input
+          class="rounded-[3.5px] px-[10.5px] py-[5.25px] text-gray-900"
+          type="file"
+          id="screenshots"
+          multiple
+          @change="handleFileUpload"
+        />
       </div>
-      <button type="submit" class="submit-button">Enviar</button>
+      <div class="flex w-full justify-center">
+        <button
+          type="submit"
+          class="duration-400 min-w-32 rounded-[3.5px] bg-[#6c757d] px-[10.5px] py-[5.25px] transition-colors hover:bg-[#5a6268]"
+        >
+          Enviar
+        </button>
+      </div>
     </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue';
+import { defineComponent, reactive, ref } from 'vue'
 
 interface FormData {
-  deviceType: string;
-  deviceModel: string;
-  operatingSystem: string;
-  otherOperatingSystem?: string;
-  appVersion: string;
-  incidentDate: string;
-  problemDescription: string;
-  reproduccionSteps: string;
-  expectedBehavior: string;
-  actualBehavior: string;
-  severityLevel: string;
-  hasPreviouslyOccurred: string;
-  additionalComments: string;
-  screenshots: File[];
+  deviceType: string
+  deviceModel: string
+  operatingSystem: string
+  otherOperatingSystem?: string
+  appVersion: string
+  incidentDate: string
+  problemDescription: string
+  reproduccionSteps: string
+  expectedBehavior: string
+  actualBehavior: string
+  severityLevel: string
+  hasPreviouslyOccurred: string
+  additionalComments: string
+  screenshots: File[]
 }
 
 export default defineComponent({
@@ -120,28 +207,28 @@ export default defineComponent({
       hasPreviouslyOccurred: '',
       additionalComments: '',
       screenshots: []
-    });
+    })
 
-    const isOtherOS = ref(false);
+    const isOtherOS = ref(false)
 
     const handleFileUpload = (event: Event) => {
-      const files = (event.target as HTMLInputElement).files;
+      const files = (event.target as HTMLInputElement).files
       if (files) {
-        form.screenshots = Array.from(files);
+        form.screenshots = Array.from(files)
       }
-    };
+    }
 
     const handleSubmit = () => {
-      console.log('Form submitted:', form);
+      console.log('Form submitted:', form)
       // Add your form submission logic here
-    };
+    }
 
     const checkOtherOS = () => {
-      isOtherOS.value = form.operatingSystem === 'Other';
+      isOtherOS.value = form.operatingSystem === 'Other'
       if (!isOtherOS.value) {
-        form.otherOperatingSystem = '';
+        form.otherOperatingSystem = ''
       }
-    };
+    }
 
     return {
       form,
@@ -149,82 +236,7 @@ export default defineComponent({
       handleFileUpload,
       handleSubmit,
       checkOtherOS
-    };
+    }
   }
-});
+})
 </script>
-
-<style scoped>
-.form-container {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-  background-color: #f5f5f5;
-  border-radius: 4px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-.form-title {
-  font-size: 24px;
-  margin-bottom: 20px;
-  text-align: center;
-}
-
-.form-group {
-  margin-bottom: 20px;
-}
-
-.form-group label {
-  display: block;
-  margin-bottom: 5px;
-  font-weight: bold;
-}
-
-.form-group input,
-.form-group textarea,
-.form-group select {
-  width: 100%;
-  padding: 10px;
-  font-size: 14px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  background-color: #fff; /* Ensure white background */
-}
-
-.submit-button {
-  width: 100%;
-  padding: 10px;
-  font-size: 16px;
-  color: #fff;
-  background-color: #007bff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-}
-
-.submit-button:hover {
-  background-color: #0056b3;
-}
-
-@media (max-width: 600px) {
-  .form-container {
-    padding: 10px;
-  }
-
-  .form-title {
-    font-size: 20px;
-  }
-
-  .form-group input,
-  .form-group textarea,
-  .form-group select {
-    font-size: 12px;
-  }
-
-  .submit-button {
-    font-size: 14px;
-  }
-}
-</style>
